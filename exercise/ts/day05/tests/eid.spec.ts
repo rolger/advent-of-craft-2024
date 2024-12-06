dce8185d3fa5863b78c2d6c2e6d8fe3e5048dae4
rgerm <roland.germ@gmail.com>
2024-12-06 16:51:35 +0100

    test: test null

--- exercise/ts/day05/tests/eid.spec.ts
+++ exercise/ts/day05/tests/eid.spec.ts
@@ -3,8 +3,13 @@ function validateEID(EID: string) {
     return true;
 }
 
-describe('EID', () => {
-    test('implement your tests here', () => {
+describe('EID Tester', () => {
+    test('valid EIDs', () => {
         expect(validateEID('19800767')).toBeTruthy();
     });
+
+    test('wrong input is an invalid EID', () => {
+        expect(validateEID(null)).toBeFalsy();
+    });
+
 });
