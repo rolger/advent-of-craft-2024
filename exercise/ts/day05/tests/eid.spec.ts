9974bf547dd2494d8eac7a06d02e1349566fb975
rgerm <roland.germ@gmail.com>
2024-12-06 17:35:24 +0100

    feat: implement year check

--- exercise/ts/day05/tests/eid.spec.ts
+++ exercise/ts/day05/tests/eid.spec.ts
@@ -1,32 +1,55 @@
 
+enum ElfSex {
+    Sloubi = 1,
+    Gagna = 2,
+    Catact = 3
+};
+
 function validateEID(EID: string) {
     if (!EID) {
         return false;
     }
 
-    if (EID.length !== 8) {
+    const VALID_LENGTH = 8;
+    if (EID.length !== VALID_LENGTH) {
         return false;
     }
+
+    let sex = EID.substring(0, 1);
+    if (!(sex in ElfSex))
+        return false;
+
+    let year = EID.substring(1, 3);
+    if (isNaN(Number(year)))
+        return false;
 
     return true;
 }
 
-describe('EID Tester', () => {
-    test('valid EIDs', () => {
+describe('EID', () => {
+    test('with valid values', () => {
         expect(validateEID('19800767')).toBeTruthy();
     });
 
-    test('empty input is an invalid EID', () => {
+    test('with empty input is invalid', () => {
         expect(validateEID(null)).toBeFalsy();
         expect(validateEID('')).toBeFalsy();
     });
 
-    test('wrong length are invalid EIDs', () => {
+    test('with wrong length are invalid', () => {
         expect(validateEID('12')).toBeFalsy();
     });
 
-    test('incorrect sex are invalid EIDs', () => {
+    test('with incorrect sex are invalid', () => {
         expect(validateEID('49800867')).toBeFalsy();
+        expect(validateEID('09800867')).toBeFalsy();
+        expect(validateEID('x9800867')).toBeFalsy();
     });
+
+    test('with incorrect year are invalid', () => {
+        expect(validateEID('21b00867')).toBeFalsy();
+    });
+
+
 
 });
