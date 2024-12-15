a7e99ad5cdb44afa1b746fb4ddcba2f47a25135a
rgerm <roland.germ@gmail.com>
2024-12-15 13:42:35 +0100

    refactor: remove unused methods in builder

--- exercise/java/day09/src/test/java/gift/ChildBuilder.java
+++ exercise/java/day09/src/test/java/gift/ChildBuilder.java
@@ -10,45 +10,24 @@ public class ChildBuilder {
     private GiftRequest giftRequest;
 
     public static ChildBuilder aNiceChild() {
-        return new ChildBuilder().setBehavior(Behavior.NICE);
+        ChildBuilder childBuilder = new ChildBuilder();
+        childBuilder.behavior = Behavior.NICE;
+        return childBuilder;
     }
 
     public static ChildBuilder aNaughtyChild() {
-        return new ChildBuilder().setBehavior(Behavior.NAUGHTY);
-    }
-
-    public ChildBuilder setFirstName(String firstName) {
-        this.firstName = firstName;
-        return this;
-    }
-
-    public ChildBuilder setLastName(String lastName) {
-        this.lastName = lastName;
-        return this;
-    }
-
-    public ChildBuilder setAge(int age) {
-        this.age = age;
-        return this;
-    }
-
-    public ChildBuilder setBehavior(Behavior behavior) {
-        this.behavior = behavior;
-        return this;
-    }
-
-    public ChildBuilder setGiftRequest(GiftRequest giftRequest) {
-        this.giftRequest = giftRequest;
-        return this;
+        ChildBuilder childBuilder = new ChildBuilder();
+        childBuilder.behavior = Behavior.NAUGHTY;
+        return childBuilder;
     }
 
     public ChildBuilder withFeasibleGift() {
-        this.setGiftRequest(new GiftRequest("Any gift", true, Priority.NICE_TO_HAVE));
+        this.giftRequest = new GiftRequest("Any gift", true, Priority.NICE_TO_HAVE);
         return this;
     }
 
     public ChildBuilder withInfeasibleGift() {
-        this.setGiftRequest(new GiftRequest("Any gift", false, Priority.DREAM));
+        this.giftRequest = new GiftRequest("Any gift", false, Priority.DREAM);
         return this;
     }
 
