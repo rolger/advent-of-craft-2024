e7b202ba2bba0f3f870611a1e68253f772f49bcb
rgerm <roland.germ@gmail.com>
2024-12-19 09:05:15 +0100

    refactor: use AssertJ for asserting Optional

--- exercise/java/day18/src/test/java/game/ChoiceTest.java
+++ exercise/java/day18/src/test/java/game/ChoiceTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -14,11 +15,8 @@ class ChoiceTest {
             "SCISSORS,rock crushes scissors",
             "LIZARD,rock crushes lizard"
     })
-    void rockShouldBeatScissorWithMessage(Choice other, String expectedMessage) {
-        Choice.ROCK.beatsWithMessage(other).ifPresentOrElse(
-                message -> assertEquals(expectedMessage, message),
-                () -> fail("ROCK should beat " + other)
-        );
+    void rockShouldBeatWithMessage(Choice other, String expectedMessage) {
+        assertThat(Choice.ROCK.beatsWithMessage(other)).hasValue(expectedMessage);
     }
 
 }
