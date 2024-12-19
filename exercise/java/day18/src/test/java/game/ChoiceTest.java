013fedf8bab50e05b427a3345e92a83e29463e37
rgerm <roland.germ@gmail.com>
2024-12-19 09:02:16 +0100

    refactor: parameterize rock tests

--- exercise/java/day18/src/test/java/game/ChoiceTest.java
+++ exercise/java/day18/src/test/java/game/ChoiceTest.java
@@ -1,25 +1,23 @@
 package game;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class ChoiceTest {
 
-    @Test
-    void rockShouldBeatScissorWithMessage() {
-        Choice.ROCK.beatsWithMessage(Choice.SCISSORS).ifPresentOrElse(
-                message -> assertEquals("rock crushes scissors", message),
-                () -> fail("rock should beat scissors")
-        );
-    }
-
-    @Test
-    void rockShouldBeatLizardWithMessage() {
-        Choice.ROCK.beatsWithMessage(Choice.LIZARD).ifPresentOrElse(
-                message -> assertEquals("rock crushes lizard", message),
-                () -> fail("rock should beat lizard")
+    @ParameterizedTest
+    @CsvSource({
+            "SCISSORS,rock crushes scissors",
+            "LIZARD,rock crushes lizard"
+    })
+    void rockShouldBeatScissorWithMessage(Choice other, String expectedMessage) {
+        Choice.ROCK.beatsWithMessage(other).ifPresentOrElse(
+                message -> assertEquals(expectedMessage, message),
+                () -> fail("ROCK should beat " + other)
         );
     }
 
