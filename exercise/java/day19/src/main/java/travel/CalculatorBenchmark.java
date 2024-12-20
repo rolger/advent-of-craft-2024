2d6fd16182bbc325f82b3e9fdbf6ee3257a17d2c
rgerm <roland.germ@gmail.com>
2024-12-20 16:02:22 +0100

    chore: reduce warmup parameters and benchmark avg time in ns

--- exercise/java/day19/src/main/java/travel/CalculatorBenchmark.java
+++ exercise/java/day19/src/main/java/travel/CalculatorBenchmark.java
@@ -5,44 +5,48 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
 import java.util.concurrent.TimeUnit;
-import java.util.function.Function;
 
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Thread)
 public class CalculatorBenchmark {
-    @Param({"RECURSIVE", "LOOP", "BITOPS"})
-    public String implementationType;
-
     @Param({"1", "10", "50"})
     public int numberOfReindeer;
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
                 .include(CalculatorBenchmark.class.getSimpleName())
-                .forks(1)
-                .warmupIterations(1)
+                .warmupTime(TimeValue.seconds(1))
                 .build();
 
         new Runner(opt).run();
     }
 
     @Benchmark
-    @Measurement(iterations = 10)
-    @BenchmarkMode(Mode.All)
-    public void calculate() {
-        createImplementation().apply(this.numberOfReindeer);
+    @Fork(1)
+    @Warmup(iterations = 5, time = 1)
+    @Measurement(iterations = 10, time = 1)
+    public void calculateRecursive() {
+        SantaTravelCalculator.calculateTotalDistanceRecursively(this.numberOfReindeer);
     }
 
-    private Function<Integer, Long> createImplementation() {
-        if (this.implementationType == "RECURSIVE") {
-            return SantaTravelCalculator::calculateTotalDistanceRecursively;
-        } else if (this.implementationType != "LOOP") {
-            return SantaTravelCalculator::calculateTotalDistanceWithBitOps;
-        }
-        return SantaTravelCalculator::calculateTotalDistance;
+    @Benchmark
+    @Fork(1)
+    @Warmup(iterations = 5, time = 1)
+    @Measurement(iterations = 10, time = 1)
+    public void calculateLoop() {
+        SantaTravelCalculator.calculateTotalDistance(this.numberOfReindeer);
+    }
+
+    @Benchmark
+    @Fork(1)
+    @Warmup(iterations = 5, time = 1)
+    @Measurement(iterations = 10, time = 1)
+    public void calculateLoopWithBitOps() {
+        SantaTravelCalculator.calculateTotalDistanceWithBitOps(this.numberOfReindeer);
     }
 
 }
