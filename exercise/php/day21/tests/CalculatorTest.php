2c51b6b35949b09f0f6711758cbacb8e74496d4d
rgerm <roland.germ@gmail.com>
2024-12-21 19:44:22 +0100

    test: add pest snapshot test

---
+++ exercise/php/day21/tests/CalculatorTest.php
@@ -0,0 +1,18 @@
+<?php
+
+use Tour\Step;
+use Tour\TourCalculator;
+
+it('calculates delivery time for some steps', function () {
+    $steps = [
+        new Step('08:00', 'Start', 0),
+        new Step('08:30', 'Location A', 1800),
+        new Step('09:00', 'Location B', 1800),
+    ];
+    
+    $calculator = new TourCalculator($steps);
+    $result = $calculator->calculate();
+    
+    expect($result)->toMatchSnapshot();
+});
+
